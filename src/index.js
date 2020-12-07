79cc4ee4a32e63fe5d3d1fcbb41595f174ef6f65
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-07 06:38:22 -0800

    Implement firebase.

--- src/index.js
+++ src/index.js
@@ -7,18 +7,35 @@ import { createStore } from 'redux';
 // import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+//next 3 added for firebase
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
 
 // const store = createStore(reducer);
 
-store.subscribe(() =>
-  console.log(store.getState())
-);
+//Next block of code added for firebase
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
+// store.subscribe(() =>
+//   console.log(store.getState())
+// );
 
 ReactDOM.render(
   <Provider store={store}>
+    {/* Next line and closing line added for firebase */}
+    <ReactReduxFirebaseProvider {...rrfProps}>
     <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
+//Next line added for firebase
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterTicketList: ticketListReducer
+  masterTicketList: ticketListReducer,
+  //next line added for firebase
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
